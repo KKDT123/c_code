afa97ab9a933d4b80d85aa4effab9f59524af3f7
KKDT123 <781837854@qq.com>
2020-07-27 16:51:13 +0600

    c_code

---
+++ day03/01_scanf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+        int a ;
+        printf("请输入：");
+        scanf("%d",&a);
+        printf("%d\n",a);
+	//这里有两个问题需要注意
+	//一个是 输入输出是d% 其次是换行符是\
+	return 0;
+       }            
